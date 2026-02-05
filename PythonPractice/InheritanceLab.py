d5c6430570548af01842072db2e420e1ed9a9b69
MKMK10 <mkravtsova@zagmail.gonzaga.edu>
2026-02-05 12:03:28 -0800

    aaa

--- PythonPractice/InheritanceLab.py
+++ PythonPractice/InheritanceLab.py
@@ -8,9 +8,47 @@ class Animal():
 
     def __str__(self) -> str:
         return f"a {self._age}-year old {self._species}"
+
+class Pet(Animal):
+    """
+    Docstring for Pet
+    """
+    def __init__(self, species: str, age: int, name: str = "default name") -> None: 
+        super().__init__(species, age)
+        self._name = name
+    
+    def __str__(self) -> None:
+        str_rep = super().__str__()
+        str_rep += " named " + self._name
+        return str_rep
+
+class WildAnimal(Animal):
+    """
+    Docstring for WildAnimal
+    """
+    def __init__(self, species: str, age: int, noise: str = "default noise") -> None: 
+        super().__init__(species, age)
+        self._ferocious_noise = noise
+    
+    def __str__(self) -> None:
+        str_rep = super().__str__()
+        str_rep += " that goes " + self._ferocious_noise
+        return str_rep
     
 def main():
     a1 = Animal()
     print(a1)
     a2 = Animal("cat",2)
     print(a2)
+
+    p1 = Pet("dog", 5, "Spike")
+    print(p1)
+
+    w1 = WildAnimal("lion", 10, "Roar")
+    print(w1)
+
+    animals = [a1, a2, p1, w1] # List of animal references.
+    for animal in animals:
+        print(animal) # Same code, different behavior.
+
+main()
